b2c9245174f78dd4b43c4001d9cc2165cc882300
Alexander <allsilaevex@gmail.com>
2018-02-13 23:18:13 +0300

    Initial Phaser

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>Labyrinth</title>
+
+        <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+
+        <script type="text/javascript" src="{{ mix('/js/app.js') }}"></script>
+        <script type="text/javascript" src="{{ mix('/js/game.js') }}"></script>
+    </head>
+    <body>
+
+    </body>
+</html>
