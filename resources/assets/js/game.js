3f0e04db543cc904f991c38889e03f846fdeb762
Unknown <orkich@gmail.com>
2018-06-25 18:07:58 +0300

    [feature] added map loading and player

--- resources/assets/js/game.js
+++ resources/assets/js/game.js
@@ -1,32 +1,68 @@
-//import Map    from "./Map";
+import Map    from "./Map";
+import Player from "./Player"
 import Phaser from "phaser-ce";
 
 
 window.onload = function() {
 
-    let game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
+    let game = new Phaser.Game(800, 800, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+    let player = null;
+    let walls = null;
+    let floor = null;
 
     function preload () {
 
-        game.load.image('logo', 'img/img.png');
+        game.load.spritesheet('player', 'img/player.png', 87, 86);
+        game.load.image('wall', 'img/wall.bmp');
+        game.load.image('grass', 'img/grass_sized.png');
 
     }
 
     function create () {
 
-        let logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-        logo.anchor.setTo(0.5, 0.5);
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        Map.testMap()
+            .then(d => drawMap(d))
+            .then(() => drawPlayer());
+    }
+
+    function update() {
+
+        game.physics.arcade.collide(player, walls);
+
+        let cursors = game.input.keyboard.createCursorKeys();
+        player.handleMoving(cursors);
 
     }
 
+    function drawMap(mapData) {
+
+        floor = game.add.group();
+        walls = game.add.group();
+        walls.enableBody = true;
+
+        for(let i = 0; i < mapData.height; ++i) {
+            for(let j = 0; j < mapData.width; ++j) {
+                let el = mapData.data[i * mapData.height + j];
+
+                switch (el) {
+                    case 1:
+                        let tile = walls.create(i*80, j*80, "wall");
+                        tile.body.immovable = true;
+                        break;
+                    default:
+                        floor.create(i*80, j*80, "grass");
+                }
+            }
+        }
+
+    }
+
+    function drawPlayer() {
+
+        player = new Player(game, 100, 100);
+
+    }
 };
 
-
-/* Example Map.get();
-
-(async () => {
-    let mapData = await Map.get();
-    console.log(mapData);
-})();
-
-*/
