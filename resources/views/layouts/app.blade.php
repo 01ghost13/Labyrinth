bbc0d6a487d0bbe970876e80ac4586fd2945df36
Alexander <allsilaevex@gmail.com>
2018-09-24 23:31:24 +0300

    [del] ratchet

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <!-- Styles -->
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
 </head>
-<body data-user-id="{{ \Auth::check() ? \Auth::user()->id : 0 }}">
+<body>
 
 <header>
     @yield('header')
